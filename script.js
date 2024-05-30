19ddde9364d0de76bda182130e05623ee0747f22
Mallisetty Surendra <malisettysurendra@gmail.com>
2024-05-30 18:13:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("mobile-menu").addEventListener("click", function() {
+        var navLinks = document.getElementById("nav-links");
+        navLinks.classList.toggle("active");
+    });
+});
+
+
+
+
+
